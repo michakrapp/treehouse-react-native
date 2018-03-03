8fa522b56d475f09f6423408620ddc21ad0933b8
Micha Krapp <github@michakrapp.de>
2018-03-04 00:08:48 +0100

    update component character

--- components/Character.js
+++ components/Character.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 
 /**
- * TODO Step 3:
+ * TODO Step 3: done
  * 1. Add Image component to display the character's image
  * 2. Add Text component to display the character's name
  * 3. Add styles to the Image and Text component
@@ -16,17 +16,35 @@ class Character extends Component{
 
     return(
       <View style={styles.container}>
-
+        <Image source={{ uri: characterThumbnail }} style={styles.image}
+        <Text style={styles.text}>
+          {name}
+        </Text>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  text: {
+    fontSize: 16,
+    alignSelf: 'center',
+    color: 'green',
+    marginTop: 25
+  }
 })
 
 Character.propTypes = {
   characterThumbnail: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired
 }
+
+export default Character
