7816fada08b01bde2c4756f844ffa29c44d44592
Micha Krapp <github@michakrapp.de>
2018-03-03 23:19:50 +0100

    add container WelcomeContainer

---
+++ containers/WelcomeContainer.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {Actions as RouteActions} from 'react-native-router-flux';
+
+import WelcomeForm from '../components/WelcomeForm';
+
+/**
+ * TODO Step 3:
+ *
+ * 1. Create WelcomeContainer component
+ * 2. Setup component class constructor
+ * 3. Add WelcomeForm component to WelcomeContainer render method.
+ * 4. Add onSubmit event handler to WelcomeForm and pass handleSubmit
+ *    component method to it
+ * 5. Add handleSubmit component method with dynamic routing using RouteActions.tabbar()
+ * 6. Bind the handleSubmit component method inside of the constuctor,
+ *    i.e this.handleSubmit = this.handleSubmit.bind(this)
+ * 7. Export WelcomeContainer component at the bottom of page
+ */
+
+class WelcomeContainer extends Component{
+  render(){
+    return(
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+})
