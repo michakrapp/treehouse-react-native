2d860572cf17b7442ea0a7a3cadb154f8d945330
Micha Krapp <github@michakrapp.de>
2018-03-04 16:48:23 +0100

    add new app.js

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
 import {Scene, Router, TabBar, Icon} from 'react-native-router-flux';
+import configureStore from './store/store'
+import { Provider } from 'react-redux'
+//1. const store = configureStore({});
+
+/**
+ * TODO Step 4:
+ *
+ * 1. Create Redux store with imported configureStore function.
+ * 2. Wrap Router Component with imported Provider Component
+ * 3. Pass the configured store to the store property of the Provider Component
+ */
 
 /*
  * Containers (Views)
@@ -15,19 +26,21 @@ import TabIcon from './components/navigation/Tab';
 
 const App = () => {
   return(
-    <Router>
-      <Scene key="root" >
-        <Scene key="Welcome" initial={true} component={WelcomeScene} title='Welcome' direction="vertical"/>
-        <Scene key="tabbar" tabs={true} tabBarStyle={{ backgroundColor: '#eee' }}>
-          <Scene key="character" title="Characters" icon={TabIcon} initial={true}>
-            <Scene key="characters" component={TeamSelectorScene} title="Characters" />
-          </Scene>
-          <Scene key="prof" title="Profile" icon={TabIcon}>
-            <Scene key="profile" component={TeamProfileScene} title="Profile" />
+    <Provider store={store}>
+      <Router>
+        <Scene key="root" >
+          <Scene key="Welcome" initial={true} component={WelcomeScene} title='Welcome' direction="vertical"/>
+          <Scene key="tabbar" tabs={true} tabBarStyle={{ backgroundColor: '#eee' }}>
+            <Scene key="character" title="Characters" icon={TabIcon} initial={true}>
+              <Scene key="characters" component={TeamSelectorScene} title="Characters" />
+            </Scene>
+            <Scene key="prof" title="Profile" icon={TabIcon}>
+              <Scene key="profile" component={TeamProfileScene} title="Profile" />
+            </Scene>
           </Scene>
         </Scene>
-      </Scene>
-    </Router>
+      </Router>
+    </Provider>
   )
 }
 
