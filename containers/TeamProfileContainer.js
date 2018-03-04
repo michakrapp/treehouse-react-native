635100ae07bd91393b4e788c4f87902c919107dc
Micha Krapp <github@michakrapp.de>
2018-03-04 17:24:10 +0100

    update container TeamProfile

--- containers/TeamProfileContainer.js
+++ containers/TeamProfileContainer.js
@@ -21,22 +21,15 @@ class TeamProfileContainer extends Component{
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      characters: {
-        1: {id: 1, name: 'Hulk', thumbnail: 'https://media.giphy.com/media/XSc4Kkc5u2WZy/giphy.gif?response_id=5924e51c9c00278bccae6381'},
-        2: {id: 2, name: 'Spiderman', thumbnail: 'https://media.giphy.com/media/wxM2SPuC0xONy/giphy.gif?response_id=5924e4fbdce001eee5377580'},
-        3: {id: 3, name: 'Wolverine', thumbnail: 'https://media.giphy.com/media/12m3hgKuSuhClW/giphy.gif?response_id=5924e4caa6a5ded54cf455f4'}
-      }
-    };
-
   }
 
   render(){
 
+    let { team } = this.props;
+
     return(
       <View style={styles.container}>
-        <CharacterList  />
+        <CharacterList characters={team.characters} />
       </View>
     )
   }
@@ -50,4 +43,16 @@ const styles = StyleSheet.create({
   }
 })
 
-export default TeamProfileContainer
+function mapStateToProps(state){
+  return {
+    team: state.team
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    Action: bindActionCreators(ActionCreators, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamProfileContainer)
