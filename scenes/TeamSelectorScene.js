4c546803b2f04c78846ed0746904d69c8e2778bf
Micha Krapp <github@michakrapp.de>
2018-03-04 01:20:59 +0100

    update TeamSelectorScene

--- scenes/TeamSelectorScene.js
+++ scenes/TeamSelectorScene.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import TeamSelectorContainer from '../containers/TeamSelectorContainer'
 
 class TeamSelectorScene extends Component{
 
   render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Team Scene</Text>
+        <TeamSelectorContainer />
       </View>
     )
   }
@@ -15,12 +16,8 @@ class TeamSelectorScene extends Component{
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
-    justifyContent: 'center',
-    backgroundColor: '#F0F3BD'
-  },
-  text:{
-    color:'blue'
+    flex: 1,
+    justifyContent: 'center'
   }
 })
 
